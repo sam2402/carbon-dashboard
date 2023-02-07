7bbba078c32d29e0ae8428c93918245f2765da07
RammBagg <ramit.bag.710@gmailcom>
2023-02-07 14:25:08 +0000

    example_bar_chart

--- frontend/src/components/BarChart.jsx
+++ frontend/src/components/BarChart.jsx
@@ -39,8 +39,8 @@ const BarChart = ({ isDashboard = false }) => {
           },
         },
       }}
-      keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
-      indexBy="country"
+      keys={["server", "PC", "employeeManagement", "others"]}
+      indexBy="continent"
       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.3}
       valueScale={{ type: "linear" }}
@@ -76,15 +76,15 @@ const BarChart = ({ isDashboard = false }) => {
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: isDashboard ? undefined : "country", // changed
+        legend: isDashboard ? undefined : "continent", // changed
         legendPosition: "middle",
-        legendOffset: 32,
+        legendOffset: 40,
       }}
       axisLeft={{
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: isDashboard ? undefined : "food", // changed
+        legend: isDashboard ? undefined : "sources", // changed
         legendPosition: "middle",
         legendOffset: -40,
       }}
@@ -121,7 +121,7 @@ const BarChart = ({ isDashboard = false }) => {
       ]}
       role="application"
       barAriaLabel={function (e) {
-        return e.id + ": " + e.formattedValue + " in country: " + e.indexValue;
+        return e.id + ": " + e.formattedValue + " in continent: " + e.indexValue;
       }}
     />
   );
