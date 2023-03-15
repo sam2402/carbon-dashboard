045c651a92940d184ddf2cfd1cea89dd9a88fe22
LiiiiGhost <tianxiang.xiong.21@ucl.ac.uk>
2023-03-15 02:27:26 +0000

    prompt change default

--- Prompt/prompt.py
+++ Prompt/prompt.py
@@ -2,6 +2,7 @@ import json
 import pandas as pd
 
 default_kWperday = 500
+clean_carbon_level = 100
 
 # Default server hardware configuration list
 server_configurations = {
@@ -67,16 +68,16 @@ def get_prompt(carbon_data):
     prompt = "Hi, we are running an project that help Volvo to reduce the carbon emisson of their sale software server. The current co2 emisson per day is " + str(carbon_emisson) + ". and The server is located in " + server_location + ". We want to reduce the carbon by these sections, can you help us getting more detail advice and explain it: "
         
     # Prioritise the use of energy that is clean 
-    if calculated_future_carbon > 100:
+    if calculated_future_carbon > clean_carbon_level:
         if server_energy_type in energy_types:
             current_energy_type = server_energy_type
             for energy_type, energy_type_carbon_value in energy_types.items():
                 calculated_future_carbon = calculated_future_carbon - energy_types[current_energy_type] + energy_type_carbon_value
-                if energy_type_carbon_value < energy_types[current_energy_type] and calculated_future_carbon < 100:
+                if energy_type_carbon_value < energy_types[current_energy_type] and calculated_future_carbon < clean_carbon_level:
                     advice_energy_type = "Use of cleaner energy sources: " + energy_type + ". "
                     prompt = prompt + advice_energy_type
                     break
-                elif energy_type == 'Nuclear Energy' and calculated_future_carbon > 100:
+                elif energy_type == 'Nuclear Energy' and calculated_future_carbon > clean_carbon_level:
                     advice_energy_type = "Use of cleaner energy sources: Nuclear Energy. "
                     prompt = prompt + advice_energy_type
                     break
@@ -87,14 +88,14 @@ def get_prompt(carbon_data):
         prompt = "Hi, we are running an project that help Volvo to reduce the carbon emisson of their sale software server. The current co2 emisson per day is " + str(carbon_emisson) +". and The server is located in "+ server_location +". The current energy type of server use is: "+ server_energy_type + ". The current cooling type of server use is: "+ server_cooling_type + ". The current server usage rate is: " + str(server_useage_rate) + ". The current server work hours per day is : " + str(server_work_time) + ". The current cpu of server use is: "+ server_configuration + ". Can you help us getting more detail advice in reduce carbon emisson and explain it."
                         
     # Increase server usage
-    if calculated_future_carbon > 100:
+    if calculated_future_carbon > clean_carbon_level:
         if server_useage_rate < 80:
             current_server_usage = server_useage_rate
             changed_server_usage = current_server_usage + 10
             while changed_server_usage <= 80:
                 changed_server_usage += 10
                 calculated_future_carbon = calculated_future_carbon * 0.85
-                if calculated_future_carbon < 100:
+                if calculated_future_carbon < clean_carbon_level:
                     advice_server_usage = "Increase the server usage rate to: "+ changed_server_usage +"%. "
                     break
         advice_server_usage = "Increase the server usage rate to: 80%. "
@@ -108,14 +109,14 @@ def get_prompt(carbon_data):
 
 
     # Reduction of server working hours/day
-    if calculated_future_carbon > 100:
+    if calculated_future_carbon > clean_carbon_level:
         if server_work_time > 18:
             current_server_work_time = server_work_time
             changed_server_work_time = current_server_work_time - 1
             while changed_server_work_time > 18:
                 changed_server_work_time -= 1
                 calculated_future_carbon = calculated_future_carbon - 300
-                if calculated_future_carbon < 100:
+                if calculated_future_carbon < clean_carbon_level:
                     advice_server_work_time = "Reduce the server work time to: "+ changed_server_work_time +". "
                     prompt = prompt + advice_server_work_time
                     break
@@ -128,16 +129,16 @@ def get_prompt(carbon_data):
         advice_server_work_time = None
 
     # Use Cleaner cooling types
-    if calculated_future_carbon > 100:
+    if calculated_future_carbon > clean_carbon_level:
         if server_cooling_type in cooling_types:
             current_cooling_type = server_cooling_type
             for cooling_type, cooling_type_carbon_value in cooling_types.items():
                 calculated_future_carbon = calculated_future_carbon - cooling_types[current_cooling_type] + cooling_type_carbon_value
-                if cooling_type_carbon_value < cooling_types[current_cooling_type] and calculated_future_carbon < 100:
+                if cooling_type_carbon_value < cooling_types[current_cooling_type] and calculated_future_carbon < clean_carbon_level:
                     advice_cooling_type = "Use of cleaner cooling type: " + cooling_type + ". "
                     prompt = prompt + advice_cooling_type
                     break
-                elif cooling_type == 'Heat pipe cooling system (water-cooled or dry-cooled)' and calculated_future_carbon > 100:
+                elif cooling_type == 'Heat pipe cooling system (water-cooled or dry-cooled)' and calculated_future_carbon > clean_carbon_level:
                     advice_cooling_type = "Use of cleaner cooling type: Heat pipe cooling system (water-cooled or dry-cooled). "
                     prompt = prompt + advice_cooling_type
                     break
@@ -148,16 +149,16 @@ def get_prompt(carbon_data):
         advice_cooling_type = None
         
     # Replace server hardware configurations that consume less energy
-    if calculated_future_carbon > 100:
+    if calculated_future_carbon > clean_carbon_level:
         if server_configuration in server_configurations:
             current_server_configuration = server_configuration
             for configuration, configuration_carbon_value in server_configurations.items():
                 calculated_future_carbon = calculated_future_carbon - server_configurations[configuration] + configuration_carbon_value
-                if configuration_carbon_value < server_configurations[configuration] and calculated_future_carbon < 100:
+                if configuration_carbon_value < server_configurations[configuration] and calculated_future_carbon < clean_carbon_level:
                     advice_server_configuration = "Change the CPU current using to: " + configuration + ". "
                     prompt = prompt + advice_server_configuration
                     break
-                elif configuration == 'Intel Xeon E5-2660 v4' and calculated_future_carbon > 100:
+                elif configuration == 'Intel Xeon E5-2660 v4' and calculated_future_carbon > clean_carbon_level:
                     advice_server_configuration = "Change the CPU current using to: Intel Xeon E5-2660 v4. "
                     prompt = prompt + advice_server_configuration
                     break
@@ -168,7 +169,7 @@ def get_prompt(carbon_data):
         advice_server_configuration = None
             
     # if predict future carbon emisson still too high cosider about moving the server location
-    if calculated_future_carbon > 100:
+    if calculated_future_carbon > clean_carbon_level:
         advice_location = "Move the server to a higher Latitude/ higher altitude region would help reduce the carbon emisson if possilbe. "
         prompt = prompt + advice_location
     else:
