0c7e4ca03c5ac1b6f36f190bb191236c6021fa4d
LiiiiGhost <tianxiang.xiong.21@ucl.ac.uk>
2023-03-19 16:48:35 +0000

    phrase prompt

--- backend/advice/prompt.py
+++ backend/advice/prompt.py
@@ -3,7 +3,9 @@ from backend.carbon.emissions import ResourceEmissionInfo
 from backend.carbon.sources.objs import cpus, location_zones
     
 def get_prompt(resource_emission_infos: list[ResourceEmissionInfo], advice_type: AdviceType) -> str:
-    prompts = []
+    
+    intro = f"Here is some information about a few Volvo servers. Please provide specific suggestions on how to reduce carbon emissions for the following servers:\n\n"   
+    server_info_prompts = []
 
     for resource_emission_info in resource_emission_infos:
         resource = resource_emission_info.resource
@@ -12,23 +14,32 @@ def get_prompt(resource_emission_infos: list[ResourceEmissionInfo], advice_type:
         power_consumption_breakdown = resource_emission_info.power_consumption_breakdown
 
         if advice_type == AdviceType.ENERGY_TYPE:
-            prompt = f"Volvo server {resource_name} has a carbon emission value of {current_carbon_emission}g from the last week, provide advice on how to change energy sources in order to decrease carbon emissions. Below is a list of each zone's breakdown of electricity usage by percentage:\n"
+            prompt = f"Volvo server {resource_name} has a carbon emission value of {current_carbon_emission}g from the last week. Below is a list of each zone's breakdown of electricity usage by percentage:\n"
             breakdown_summary = ', '.join([f"{k}: {v}" for k, v in power_consumption_breakdown.items()])
             prompt += f"{breakdown_summary}\n"
 
         elif advice_type == AdviceType.LOCATION:
             location_name = location_zones[resource.location]["name"]
-            prompt = f"Volvo server {resource_name} is currently located at {location_name} and last week's carbon emission value is {current_carbon_emission}g, give some suggested locations in order to decrease carbon emissions.\n"
+            prompt = f"Volvo server {resource_name} is currently located at {location_name} and last week's carbon emission value is {current_carbon_emission}g.\n"
 
         elif advice_type == AdviceType.RESOURCE_CONFIGURATION:
             cpu_tier = resource.sku.tier if resource.sku is not None and resource.sku.tier is not None else "default"
             cpu_model = cpus[cpu_tier]["model"]
             power_rating = cpus[cpu_tier]["power"]
-            prompt = f"Volvo server {resource_name} is currently using a {cpu_model} CPU with a power rating of {power_rating} watts and last week's carbon emission value is {current_carbon_emission}g, could you please give some exact suggested cpu types with lower power ratings but similar compatibility to replace the current one then it can decrease carbon emissions.\n"
+            prompt = f"Volvo server {resource_name} is currently using a {cpu_model} CPU with a power rating of {power_rating} watts and last week's carbon emission value is {current_carbon_emission}g.\n"
 
         elif advice_type == AdviceType.COOLING_TYPE:
-            prompt = f"Volvo server {resource_name} has a carbon emission value of {current_carbon_emission}g from the last week. Could you please suggest a suitable and efficient cooling type for the current server, along with an approximate cost estimation.\n"
+            prompt = f"Volvo server {resource_name} has a carbon emission value of {current_carbon_emission}g from the last week.\n"
 
-        prompts.append(prompt)
+        server_info_prompts.append(prompt)
+        
+    if advice_type == AdviceType.ENERGY_TYPE:
+        request_advice_prompt = "Provide advice on how to change energy sources for each server in order to decrease carbon emissions."
+    elif advice_type == AdviceType.LOCATION:
+        request_advice_prompt = "Give some suggested locations for each server in order to decrease carbon emissions."
+    elif advice_type == AdviceType.RESOURCE_CONFIGURATION:
+        request_advice_prompt = "Could you please give some exact suggested cpu types with lower power ratings but similar compatibility to replace the current one for each server then it can decrease carbon emissions."
+    elif advice_type == AdviceType.COOLING_TYPE:
+        request_advice_prompt = "Could you please suggest a suitable and efficient cooling type for each server, along with an approximate cost estimation."
 
-    return "\n".join(prompts)
+    return intro + "\n".join(server_info_prompts) + request_advice_prompt
