c1d14c8f04c80da04d81096df3397a7ede288237
JUNZHANG LI <91993785+JUNZHANG-Li@users.noreply.github.com>
2023-03-20 23:12:23 +0000

    Update Footer.jsx

--- frontend/src/scenes/global/Footer.jsx
+++ frontend/src/scenes/global/Footer.jsx
@@ -1,20 +1,68 @@
-import { Box, IconButton, useTheme } from "@mui/material";
+import { useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext } from "../../theme";
-import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+
+import React from "react";
+import {
+  Box,
+  Container,
+  Row,
+  Column,
+  FooterLink,
+  Heading,
+} from "./FooterStyles";
 
 const Footer = () => {
   const theme = useTheme();
   const colorMode = useContext(ColorModeContext);
 
   return (
-    <Box display="flex" justifyContent="space-between">
-      <Box p={2}>
-
+      <Box 
+        display="flex" 
+        justifyContent="space-between"
+        >
+        <h4 style={{ color: "White", 
+                    textAlign: "center", 
+                    marginTop: "-40px" }}>
+          Volvo - "We make cars for people who care about other people."
+        </h4>
+        <Container>
+          <Row>
+            <Column>
+              <Heading>About Us</Heading>
+              <FooterLink href="#">Aim</FooterLink>
+              <FooterLink href="#">Vision</FooterLink>
+              <FooterLink href="#">Testimonials</FooterLink>
+            </Column>
+            <Column>
+              <Heading>Contact Us</Heading>
+              <FooterLink href="#">UCL</FooterLink>
+              <FooterLink href="#">Avanade</FooterLink>
+              <FooterLink href="#">Volvo</FooterLink>
+            </Column>
+            <Column>
+              <Heading>Social Media</Heading>
+              <FooterLink href="#">
+                <i className="fab fa-facebook-f">
+                  <span style={{ marginLeft: "10px" }}>
+                    Facebook
+                  </span>
+                </i>
+              </FooterLink>
+              <FooterLink href="#">
+                <i className="fab fa-twitter">
+                  <span style={{ marginLeft: "10px" }}>
+                    Twitter
+                  </span>
+                </i>
+              </FooterLink>
+            </Column>
+          </Row>
+        </Container>
         
       </Box>
-    </Box>
+
+   
   );
 };
 
